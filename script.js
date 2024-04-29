cd30565f60bbc95dbb74d440653fa2164c9061a2
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2024-04-29 09:36:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-function findLargest(a, b, c) {
+*function findLargest(a, b, c) {
   //your code here
+	if (a >= b && a >= c) {
+		return a;
+	} 
+	else if (b >= a && b >= c) {
+		return b;
+	} 
+	else {
+		return c;
+	}
 }
 
 const num1 = parseInt(prompt("Enter First Number."));
